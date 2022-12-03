b003483981281ae22fed8092ca093584d910c108
AlexDolz <aleksejdolznikov3@gmail.com>
2022-12-03 11:11:23 +0100

    gallery-slider

--- gallery-slider.js
+++ gallery-slider.js
@@ -1,6 +1,6 @@
 let setPx = 0;
 
-const sliderLine = document.querySelector('#slider_line');
+const sliderRow = document.querySelector('#slider_line');
 
 const nextBtn = document.querySelector('#slider_next');
 const prevBtn = document.querySelector('#slider_prev');
@@ -10,7 +10,7 @@ nextBtn.addEventListener('click', () => {
   if (setPx > 2136) {
     setPx = 0;
   }
-  sliderLine.style.right = setPx + 'px';
+  sliderRow.style.right = setPx + 'px';
 });
 
 prevBtn.addEventListener('click', () => {
@@ -18,5 +18,5 @@ prevBtn.addEventListener('click', () => {
   if (setPx < 0) {
     setPx = 2136;
   }
-  sliderLine.style.right = setPx + 'px';
+  sliderRow.style.right = setPx + 'px';
 });
